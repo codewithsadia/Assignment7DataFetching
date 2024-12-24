19666d6c87add53664f0e4dd5d72e022e7040b19
Sadia Imran <164052169+codewithsadia@users.noreply.github.com>
2024-12-25 00:58:56 +0500

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='font-serif'>
+       <h1 className=' gradient bg-gradient-to-t to-slate-700 from-slate-300 text-blue-950 font-small text-center h-90'>
+       Made by Sadia Imran 
+       <br />
+       All rights reserved.<br>
+      </br> contact me via email: sadiaimranmalik24@gmail.com
+      </h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ Navbar.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { DiCodeigniter } from "react-icons/di";
+import { IoIosHome } from "react-icons/io";
+import { FaLaptop } from "react-icons/fa";
+import { HiMiniAdjustmentsHorizontal } from "react-icons/hi2";
+
+const Navbar = () => {
+  return (
+    <header className="bg-slate-400 text-black">
+      {/* Top Section */}
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center px-4 py-2 gap-4">
+        <div className="font-serif text-15px font-bold">
+          <h1 className="hover:underline">Data Fetching</h1>
+        </div>
+        <div className="flex justify-end items-center gap-4 font-serif">
+          <p className="text-sm text-black hover:underline">Help</p>
+          <span className="hidden sm:block border-l border-black h-5"></span>
+          <p className="text-sm text-black hover:underline">Join Us</p>
+          <span className="hidden sm:block border-l border-black h-5"></span>
+          <p className="text-sm text-black hover:underline">Sign In</p>
+        </div>
+      </div>
+
+      {/* Bottom Section */}
+      <div className="grid grid-cols-1 md:grid-cols-[auto_1fr] items-center bg-slate-200 px-4 py-2 gap-4">
+        {/* Logo Section */}
+        <div className="flex items-center">
+        <DiCodeigniter className="mr-2"  />
+        </div>
+
+        {/* Navigation Section */}
+        <nav className="grid grid-flow-col auto-cols-max gap-4 justify-self-end font-serif text-lg">
+          <Link href="/" aria-label="Home" className="flex items-center px-4 hover:underline">
+            <IoIosHome className="mr-2" />
+            <span>Home</span>
+          </Link>
+          <Link
+            href="/client"
+            aria-label="Client Side"
+            className="flex items-center px-4 hover:underline"
+          >
+            <FaLaptop className="mr-2" />
+            <span>Client Side</span>
+          </Link>
+          <Link
+            href="/server"
+            aria-label="Server Side"
+            className="flex items-center px-4 hover:underline"
+          >
+            <HiMiniAdjustmentsHorizontal className="mr-2" />
+            <span>Server Side</span>
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
