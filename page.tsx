ee9ff8bd0b19bd974b4e3988dd5adb25ec7154a8
Sadia Imran <164052169+codewithsadia@users.noreply.github.com>
2024-12-25 00:59:52 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,12 +1,13 @@
-
 import React from 'react'
 
-const Client = () => {
+const Server = () => {
   return (
-    <div>
-      <h1>Client Side</h1>
+    <div className='w-15px h-20 bg-slate-500 p-5 m-4 rounded-full'>
+       <h1 className='grid justify-center text-3xl font-serif'>
+      Server Side Data Fetching
+      </h1>
     </div>
   )
 }
 
-export default Client
+export default Server
